9fdc1b66538cc3913c765262ac94b99f830e3e9e
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2022-08-09 21:32:10 +0900

    Update qsub.bat

--- qsub.bat
+++ qsub.bat
@@ -1,12 +1,12 @@
-#!/bin/bash                                                                                                                   
-#                                                                                                                             
-#$ -cwd                                                                                                                       
-#$ -j y                                                                                                                       
-#$ -S /bin/bash                                                                                                               
-#$ -M kawasaki@r.phys.nagoya-u.ac.jp                                                                                          
-#$ -m ea                                                                                                                      
-#$ -V                                                                                                                         
-#                                                                                                                             
-#$ -q all.q@banana                                                                                                            
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+#$ -M kawasaki@r.phys.nagoya-u.ac.jp
+#$ -m ea
+#$ -V
+#
+#$ -q all.q@banana
 
 ./a.out
